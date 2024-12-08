fd299ec076c9656af3514c199196ed143aaa6139
Geonwoo Kim <geonwkim@c2r8s4.42tokyo.jp>
2024-12-08 23:03:20 +0900

    [Feature] - ex02

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/08 22:54:46 by geonwkim          #+#    #+#             */
+/*   Updated: 2024/12/08 22:59:19 by geonwkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <cmath>
+# include <iostream>
+
+class Fixed
+{
+	private:
+		int _fixedPointValue;	// To store the fixed-point number
+		static const int _fractionalBits = 8; // The number of fractional bits
+	public:
+		Fixed();
+		Fixed(const int intValue);
+		Fixed(const float floatValue);
+		Fixed(const Fixed& fixed);
+		~Fixed();
+		Fixed& operator=(const Fixed& fixed);
+
+		// Comparison operators
+		bool operator>(const Fixed& fixed) const;
+		bool operator<(const Fixed& fixed) const;
+		bool operator>=(const Fixed& fixed) const;
+		bool operator<=(const Fixed& fixed) const;
+		bool operator==(const Fixed& fixed) const;
+		bool operator!=(const Fixed& fixed) const;
+
+		Fixed operator+(const Fixed& fixed) const;
+		Fixed operator-(const Fixed& fixed) const;
+		Fixed operator*(const Fixed& fixed) const;
+		Fixed operator/(const Fixed& fixed) const;
+
+		Fixed operator++();	// Pre-increment
+		Fixed operator++(int);	// Post-increment
+		Fixed operator--();	// Pre-decrement
+		Fixed operator--(int);	// Post-decrement
+
+		// Member functions
+		float toFloat(void) const;
+		int toInt(void) const;		
+
+		// Static member function
+		static Fixed& min(Fixed& a, Fixed& b);
+		static const Fixed& min(const Fixed& a, const Fixed& b);
+		static Fixed& max(Fixed& a, Fixed& b);
+		static const Fixed& max(const Fixed& a, const Fixed& b);
+
+		// Getters and Setters
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
+// Overloaded insertion (<<) operator
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
+
+#endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,28 +6,23 @@
 /*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 23:08:24 by geonwkim          #+#    #+#             */
-/*   Updated: 2024/12/08 22:54:31 by geonwkim         ###   ########.fr       */
+/*   Updated: 2024/12/08 23:01:37 by geonwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main() {
+int main( void ) {
     Fixed a;
-    Fixed const b(10);
-    Fixed const c(42.42f);
-    Fixed const d(b);
-    a = Fixed(1234.4321f);
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ));
 
-    std::cout << "a is " << a << std::endl;
-    std::cout << "b is " << b << std::endl;
-    std::cout << "c is " << c << std::endl;
-    std::cout << "d is " << d << std::endl;
-
-    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
 
     return 0;
 }
